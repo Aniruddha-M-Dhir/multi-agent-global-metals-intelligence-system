c28f12cb0aed96c909c511c05335017a21c05291
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2025-11-30 18:40:59 -0800

    Update arbitrage_utils.py

--- src/tools/arbitrage_utils.py
+++ src/tools/arbitrage_utils.py
@@ -1,1 +1,20 @@
+"""
+Arbitrage Utility Tool
+Helper functions to support arbitrage detection.
+"""
+
+def calculate_spread(low_price, high_price):
+    """
+    Returns the absolute price spread.
+    """
+    return round(high_price - low_price, 2)
+
+def calculate_spread_percent(low_price, high_price):
+    """
+    Returns the percentage spread between two prices.
+    """
+    if low_price == 0:
+        return 0
+    spread = high_price - low_price
+    return round((spread / low_price) * 100, 2)
 
